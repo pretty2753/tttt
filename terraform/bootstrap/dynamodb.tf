1fa7f10f4ec9a0f71a6a99f9ac5847965fc4b25a
minki <songminki1@naver.com>
2026-05-19 23:42:19 +0900

    s3 dynamo 오타 수정

--- terraform/bootstrap/dynamodb.tf
+++ terraform/bootstrap/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "dream-team-terraform-lock-mk"
+  name         = "dream-team-terraform-lock"
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
